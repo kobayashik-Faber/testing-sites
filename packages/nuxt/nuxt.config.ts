daec1a5ac9c847624d511bc4b54cfd7020be3889
Masayuki Sugahara <106807210+sugahara-Faber@users.noreply.github.com>
2024-10-11 12:11:35 +0900

    update opt tag

--- packages/nuxt/nuxt.config.ts
+++ packages/nuxt/nuxt.config.ts
@@ -7,9 +7,9 @@ export default defineNuxtConfig({
         {
           id: "mierucaOptimizejs",
           innerHTML: `
-window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1842686338]);
+window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1150133615]);
 (function () {var fjs = document.createElement('script');fjs.type = 'text/javascript';
-fjs.async = true;fjs.id = "fjssync";var timestamp = new Date;fjs.src = 'https://opt.mieru-ca.com/service/js/mieruca-optimize.js?v=' + timestamp.getTime();
+fjs.async = true;fjs.id = "fjssync";var timestamp = new Date;fjs.src = 'https://dev.opt.mieru-ca.com/service/js/mieruca-optimize-dev.js?v=' + timestamp.getTime();
 var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(fjs, x);})();
           `,
         },
