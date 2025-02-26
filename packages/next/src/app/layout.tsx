87ad15437c3c13d2aa241b5c1734bea71cf175e6
Kota Kobayashi <kota.kobayashi@fabercompany.co.jp>
2025-02-26 13:45:12 +0900

    📈 Use gtm instead

--- packages/next/src/app/layout.tsx
+++ packages/next/src/app/layout.tsx
@@ -19,17 +19,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Script
-        id="mierucaOptimizejs"
+        id="gtm"
         dangerouslySetInnerHTML={{
           __html: `
-window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1842686338]);
-(function () {var fjs = document.createElement('script');fjs.type = 'text/javascript';
-fjs.async = true;fjs.id = "fjssync";var timestamp = new Date;fjs.src = 'https://opt.mieru-ca.com/service/js/mieruca-optimize.js?v=' + timestamp.getTime();
-var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(fjs, x);})();
-      `,
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W6Z5HJBL');
+        `,
         }}
       />
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6Z5HJBL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          `,
+          }}
+        ></noscript>
         <AppHeader />
         {children}
         <AppFooter />
